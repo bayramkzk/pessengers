08744aee7ad7c3e15e0b012b5f181d4f85c91d1d
highberg <48856944+highberg@users.noreply.github.com>
2022-08-08 05:42:10 +0300

    Fix Header hydration with dynamic import

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,16 @@
 import AppBar from "@mui/material/AppBar";
 import Container from "@mui/material/Container";
 import Toolbar from "@mui/material/Toolbar";
+import dynamic from "next/dynamic";
 import LogoLabelDesktop from "./LogoLabelDesktop";
 import LogoLabelMobile from "./LogoLabelMobile";
 import NavBar from "./NavBar";
 import NavMenu from "./NavMenu";
-import ProfileMenu from "./ProfileMenu";
+
+const ProfileMenu = dynamic(() => import("./ProfileMenu"), { ssr: false });
 
 const pages = [
-  { name: "Flights", href: "/flights" },
+  { name: "Flights", href: "/" },
   { name: "Profile", href: "/profile" },
   { name: "About", href: "/about" },
 ];
