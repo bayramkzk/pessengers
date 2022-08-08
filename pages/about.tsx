74fca2d3ca459ad87cc6dd6d4c64ecc0bc1156f3
highberg <48856944+highberg@users.noreply.github.com>
2022-08-08 07:43:38 +0300

    Escape quotes in about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -16,7 +16,7 @@ const AboutPage: NextPage = () => {
           }}
         >
           <Typography
-            variant="h3"
+            variant="h4"
             fontWeight={700}
             textAlign="center"
             marginBottom={2}
@@ -29,8 +29,8 @@ const AboutPage: NextPage = () => {
             matching passengers by their interests so they can have a more
             comfortable travel with a person who have common with them. Thus,
             they can share knowledge, experience and talk about their favorite
-            film series or any topic they have in common. They can be "travel
-            friends".
+            film series or any topic they have in common. They can be
+            &quot;travel friends&quot;.
           </Typography>
 
           <Typography maxWidth={700}>Copyright 2023, Rapture AISub</Typography>
